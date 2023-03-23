296349525bf34ad2ac96ab2e88aba5fc3f6e3ab5
Borisenko Valentin <ext-vborisenko@ozon.ru>
2023-03-23 18:28:55 +0700

    initial

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $allTodos = Todo::all();
+
+        dd($allTodos);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $fakeTasks = [
+            [
+                'title' => 'Сделать доманку по Laravel',
+                'description' => 'Разработать простое Todo приложение',
+                'done' => true
+            ],
+            [
+                'title' => 'Переписать домашку на RESTFULL API',
+                'description' => 'Todo приложение должно работатька SPA на React JS',
+                'done' => false
+            ]
+        ];
+
+        foreach($fakeTasks as $task) {
+            Todo::create($task);
+        }
+
+        return redirect('todo');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo, $id)
+    {
+        $task = Todo::find($id);
+        dd($task);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_03_23_085947_create_todos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->default('Новая задача');
+            $table->text('description')->default('Описание задачи…');
+            $table->boolean('done')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
